339cb3eb21c66a6436694e3d3863c251b478ce39
Corvax <vlvete1990@yandex.ru>
2020-04-30 15:34:41 +0300

    adding folders arhitecture

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+let path = require('path');
+
+let conf = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    }
+};
+
+module.exports = conf;
