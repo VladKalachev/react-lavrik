35299c41e32357abb116e1dba22a11c7fc5bad28
Corvax <vlvete1990@yandex.ru>
2020-05-02 19:43:16 +0300

    addin 2-input file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HW1 from './hw/1-simple';
+import HW2 from './hw/2-input';
 
 export default function() {
     return(
         <div>
             <h2>Min max simple</h2>
-            <HW1 min={1} max={5} />
+            <HW1 min={1} max={"5"} />
         </div>
     )
 };
