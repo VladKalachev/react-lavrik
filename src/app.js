4f7628f9f470c742f8535df6e44ca5ebce5e0ac7
Corvax <vlvete1990@yandex.ru>
2020-05-03 17:11:53 +0300

    adding 3-lazy file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import HW1 from './hw/1-simple';
 import HW2 from './hw/2-input';
+import HW3 from './hw/3-lazy';
 
 export default function() {
     return(
         <div>
             <h2>Min max simple</h2>
-            <HW1 min={1} max={"5"} />
+            <HW1 min={1} max={5} />
+            <h2>Min max input</h2>
+            <HW2 min={1} max={5} />
+            <h2>Min max lazy</h2>
+            <HW3 min={20} max={50} />
         </div>
     )
 };
--- src/hw/3-lazy.js
+++ src/hw/3-lazy.js
@@ -14,7 +14,8 @@ export default class Some extends React.Component {
     }
     
     state = {
-        cnt: this.props.min
+        cnt: this.props.min,
+        inputValue: this.props.min
     };
 
     increase = () => {
@@ -27,19 +28,40 @@ export default class Some extends React.Component {
 
     set(newCnt){
         let cnt = Math.min(Math.max(newCnt, this.props.min), this.props.max);
-        this.setState({ cnt })
+        this.setState({ 
+            cnt,
+            inputValue: cnt
+        })
     }
 
     setValue(newStr){
-        let cnt = parseInt(newStr);
+        this.setState({
+            inputValue: newStr
+        });
+    }
+
+    applayValue = () => {
+        let cnt = parseInt(this.state.inputValue);
         this.set(isNaN(cnt) ? this.props.min : cnt);
+    }
+
+    checkEnterKey = (e) => {
+        if(e.keyCode === 13){
+            this.applayValue();
+        }
     }
 
     render(){
         return(
             <div>
+                {this.state.cnt}<br/>
+                {this.state.inputValue}<br/>
                 <button onClick={this.decrease}> - </button>
-                <input value={this.state.cnt} onChange={(e) => this.setValue(e.target.value)}/>
+                <input value={this.state.inputValue} 
+                        onChange={(e) => this.setValue(e.target.value)}
+                        onBlur={this.applayValue}
+                        onKeyUp={this.checkEnterKey}
+                />
                 <button onClick={this.increase}> + </button>
             </div>
         )
